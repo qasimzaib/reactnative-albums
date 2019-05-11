f81ac264f48994fe7fc5ec57a4857cebd8637135
Archer <zaib_qasim@hotmail.com>
2019-05-11 22:56:18 +0500

    Initial config

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+const App = () => (
+	<Text>Hello World!</Text>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('albums', () => App);
