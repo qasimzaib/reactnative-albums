869222aabaeb0521fc272ced3c59fc5c6e150da3
Archer <zaib_qasim@hotmail.com>
2019-05-19 17:13:36 +0500

    Create album list component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+	return (
+		<View>
+			<Text>Album list</Text>
+		</View>
+	);
+};
+
+export default AlbumList;
