2b17eb12acaf2f9d5829ef0f58034aafbc88968a
Archer <zaib_qasim@hotmail.com>
2019-05-19 16:06:25 +0500

    Dynamic header text

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => {
-	const { textStyle } = styles;
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
 
-	return <Text style={textStyle}>Albums</Text>;
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{ props.headerText }</Text>
+		</View>
+	);
 };
 
 const styles = {
+	viewStyle: {
+		backgroundColor: '#006600',
+		justifyContent: 'center',
+		height: 60,
+		paddingLeft: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 1,
+		elevation: 4,
+		position: 'relative'
+	},
 	textStyle: {
+		color: '#FFFFFF',
 		fontSize: 20
 	}
 };
